f434489809efc02d8c2d88dc0566d0e88a8914a1
Angélique Faye <faye.angelique.d@gmail.com>
2020-06-18 13:06:01 +0200

    legibility

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-setInterval(() => {
-    // solution found here https://stackoverflow.com/questions/2917175/return-multiple-values-in-javascript
-    let {
-      currentHour,
-      currentMinutes,
-      currentSeconds
-    } = (function getCurrenttime() {
-      var today = new Date();
-      var currentHour = today.getHours();
-      var currentMinutes = today.getMinutes();
-      var currentSeconds = today.getSeconds();
-      return { currentHour, currentMinutes, currentSeconds };
-    })();
+const displayTime = function(){
+    function getCurrenttime(){
+        let hourHand = document.querySelector(".hand.hour-hand");
+        let minuteHand = document.querySelector(".hand.min-hand");
+        let secondsHand = document.querySelector(".hand.second-hand");
+        
+        let currentHour;
+        let currentMinutes;
+        let currentSeconds;
 
-    var hourHand = document.querySelector(".hand.hour-hand");
-    var minuteHand = document.querySelector(".hand.min-hand");
-    var secondsHand = document.querySelector(".hand.second-hand");
+        let today = new Date();
 
-    hourRotation = 90 + currentHour * 30;
-    minuteRotation = 90 + currentMinutes * 6;
-    secondsRotation = 90 + currentSeconds * 6;
-
-    hourHand.style.transform = "rotate(" + hourRotation + "deg)";
-    minuteHand.style.transform = "rotate(" + minuteRotation + "deg)";
-    secondsHand.style.transform = "rotate(" + secondsRotation + "deg)";
-  }, 100);
+        currentHour = today.getHours();
+        currentMinutes = today.getMinutes();
+        currentSeconds = today.getSeconds();
+        
+        hourRotation = 90 + currentHour * 30;
+        minuteRotation = 90 + currentMinutes * 6;
+        secondsRotation = 90 + currentSeconds * 6;
+        
+        hourHand.style.transform = "rotate(" + hourRotation + "deg)";
+        minuteHand.style.transform = "rotate(" + minuteRotation + "deg)";
+        secondsHand.style.transform = "rotate(" + secondsRotation + "deg)";
+    }
+    setInterval(() => {
+        getCurrenttime()
+    }, 100);
+}
+displayTime()
